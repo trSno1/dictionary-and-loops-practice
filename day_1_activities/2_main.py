a9affb7278c85173f7f414a4620982cd0beb3490
trSno1 <aguilarjayden93@gmail.com>
2025-12-15 14:51:21 -0600

    Update 2_main.py

--- day_1_activities/2_main.py
+++ day_1_activities/2_main.py
@@ -1,3 +1,5 @@
+
+
 # This activity:
 #     Reinforces nested dictionaries & lists
 #     Ties directly to real school systems
@@ -41,12 +43,26 @@
     #     Why does students[0]['Email'][0] use two sets of brackets?
 import student_data
 
-# print(student_data.students)
+# # print(student_data.students)
 students = student_data.students
-print(len(students))
-print(students[0]['Combo,Name'])
-print(students[0]['Email'][0])
-print(students[0]['Email'][1])
+# print(len(students))
+# print(students[0]['Combo,Name'])
+# print(students[0]['Email'][0])
+# print(students[0]['Email'][1])
+
+# #0
+# print(len(students))
+# print(students[1]['Combo,Name'])
+# print(students[1]['Email'][0])
+# print(students[1]['Email'][1])
+# #get the 2nd students cpsid,1st email, grade level
+
+# print(len(students))
+# print(students[1]['CPSID'][1])
+# print(students[1]['Email'][0])
+# print(students[1]['GL'][1])
+
+
 
 # What is being counted?
 
@@ -60,92 +76,113 @@ print(students[0]['Email'][1])
 
 
 
-# for loops allow us to
-#iterate through the data
-#and perform some function
+# # for loops allow us to
+# #iterate through the data
+# #and perform some function
 
-#we are iterating through the data
-#and printing the name and email of the students
-#we are also printing a line of underscores to separate the students
-#we are also printing a line of underscores to separate the students
-for student in students:
-    print(student['Combo,Name'])
-    print(student['Email'][0])
-    print(student['Email'][1])
-    print("_"*25)
+# # #we are iterating through the data
+# # #and printing the name and email of the students
+# # #we are also printing a line of underscores to separate the students
+# # #we are also printing a line of underscores to separate the students
+# for student in students:
+#     print(student['Combo,Name'])
+#     print(student['Email'][0])
+#     print(student['Email'][1]) 
+#     #get the hr, gl, and cpsid
+#     print("_"*25)
+#     print(student['HR'])
+#     print(student['GL'])
+#     print(student['CPSID'])
+#     if student['GL'] >= 10:
+#         print("You can go off campus")
 
-# What does the loop variable student represent on each pass?
+# if student['LName'][0] =="R":
+#     print("your name starts with R")
+#     print(student['Combo,Name'])
+# #     print(student['HR'])
+# #     print("_"+25)
+    
+    
+    
+# if student['LName'][0] =="L":
+#     break
+# print("_"*25)
+# name = input("what is you name?")
+# for student in students:
+#     if name == students['Combo']
 
-# How many times will this loop run?
+# # What does the loop variable student represent on each pass?
 
-# Why does each student print two emails?
+# # How many times will this loop run?
 
-# What is the purpose of the line of underscores?
+# # Why does each student print two emails?
 
-
-
-
-# we are asking the user to input their name
-# then we are checking if the name is in the data
-# if the name is in the data we are printing the name and "this works"
-name = input("what is you name?") 
-for student in students:
-    if name == student['Combo,Name']:
-        print(student['Combo,Name'])
-        print("this works")
-
-# What is the goal of this search?
-
-# What happens if the name is not found?
-
-# Why is the comparison happening inside the loop?
-
-# What would happen if two students had the same name?
-
-
-
+# # What is the purpose of the line of underscores?
 
 
 
 
+# # we are asking the user to input their name
+# # then we are checking if the name is in the data
+# # if the name is in the data we are printing the name and "this works"
+# name = input("what is you name?") 
+# for student in students:
+#     if name == student['Combo,Name']:
+#         print(student['Combo,Name'])
+#         print("this works")
 
-# practice challenge: 
+# # What is the goal of this search?
 
-# Let's try to print out the emails of the students:    
+# # What happens if the name is not found?
 
-# how  would you use this dataset to:
+# # Why is the comparison happening inside the loop?
 
-# Email all students in homeroom B233
-
-# List all students in grade 10
-
-# Find the CPS ID of a student when given only their name
-
-# Send an email to only the second email address of every student
+# # What would happen if two students had the same name?
 
 
 
 
-# Modify the Dataset 
 
-#  describe how you  would:
 
-        # Update a student who changed homeroom
 
-        # Add a new student to the school
 
-        # Remove a student who transferred out
+# # practice challenge: 
 
-        # Correct a student whose last name was misspelled
+# # Let's try to print out the emails of the students:    
 
-# Challenge Scenarios (Higher-Order Thinking)
+# # how  would you use this dataset to:
 
-    # Students respond in writing:
+# # Email all students in homeroom B233
 
-    # The district wants to pull all students whose last name starts with “M.”
-    # → How would the system search for this?
+# # List all students in grade 10
 
-    # An email system crashes because one student is missing an Email key.
-    # → What does that tell you about real-world data validation?
+# # Find the CPS ID of a student when given only their name
 
-    # Why would a dictionary be faster than a list for finding one student instantly?
+# # Send an email to only the second email address of every student
+
+
+
+
+# # Modify the Dataset 
+
+# #  describe how you  would:
+
+#         # Update a student who changed homeroom
+
+#         # Add a new student to the school
+
+#         # Remove a student who transferred out
+
+#         # Correct a student whose last name was misspelled
+
+# # Challenge Scenarios (Higher-Order Thinking)
+
+#     # Students respond in writing:
+
+#     # The district wants to pull all students whose last name starts with “M.”
+#     # → How would the system search for this?
+
+#     # An email system crashes because one student is missing an Email key.
+#     # → What does that tell you about real-world data validation?
+
+#     # Why would a dictionary be faster than a list for finding one student instantly?
